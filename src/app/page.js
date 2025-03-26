0d636a921a8381b09ea873a516e708f44790ada6
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-25 23:55:42 -0300

    03-Quick Tailwind
    03-Quick Tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 
 export default function Home() {
   return (
-    <>
-      HOME
-    </>
+    <div className="container mx-auto px-4">
+        <div className="text-center text-blue-950 text-2xl border-2 border-red-500 rounded-md bg-orange-300 hover:bg-violet-700">
+          Hello guys
+        </div>
+        <hr/>
+        <div className="card">
+          Card
+        </div>
+
+    </div>
   );
 }
