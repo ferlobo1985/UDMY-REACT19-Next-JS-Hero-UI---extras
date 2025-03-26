903b2e0ef7a22252c6750ca24c6f02123d98ab8d
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-26 00:02:11 -0300

    04-Sass
    04-Sass

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "../../styles/globals.scss"
 import {HeroUIProvider} from "@heroui/react";
 
 const geistSans = Geist({
