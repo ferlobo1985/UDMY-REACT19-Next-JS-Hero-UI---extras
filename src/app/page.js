cbf0fdcda2711448269d2b3746a5cf58c71671e6
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-26 00:10:58 -0300

    05-Next UI introduction
    05-Next UI introduction

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
 
-        <div className="someClass">
-          SASS
-        </div>
 
     </div>
   );
